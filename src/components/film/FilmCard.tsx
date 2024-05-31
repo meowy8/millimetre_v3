d52dd8f51d828a61c116a0ee3e8b0135555ae84f
meowy8 <mhoey8@hotmail.com>
2024-05-31 20:18:42 +0100

    fix film cards not displaying text

--- src/components/film/FilmCard.tsx
+++ src/components/film/FilmCard.tsx
@@ -10,18 +10,22 @@ const FilmCard = ({
   filmId,
 }: FilmCardProps) => {
   return (
-    <Link href={`/film/${filmId}`} className="relative rounded-lg shadow-lg">
-      <div className="bg-black w-full h-[20%] absolute bottom-0 blur-lg"></div>
-      <div className="absolute bottom-5 flex items-end gap-4 outfit mx-4">
-        <span className="text-2xl line-clamp-1">{title}</span>
+    <Link
+      href={`/film/${filmId}`}
+      className="relative rounded-lg shadow-lg overflow-hidden"
+    >
+      <div className="absolute bottom-0 w-full h-40 bg-gradient-to-t from-black to-transparent"></div>
+      <div className="absolute bottom-4 left-4 right-4 text-white outfit flex flex-col items-start gap-2">
+        <span className="text-2xl font-light line-clamp-1">{title}</span>
         <span className="text-lg">{releaseDate.split("-")[0]}</span>
       </div>
       <Image
         src={`https://image.tmdb.org/t/p/w500${backdropPath}`}
         alt="film poster"
-        width={390}
-        height={224}
-        className="w-full h-full object-cover rounded-lg"
+        layout="responsive"
+        width={500}
+        height={281}
+        className="w-full h-full object-cover"
       />
     </Link>
   );
